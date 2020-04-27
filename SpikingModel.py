6c918c783648c926107d19c082694b062d3558e0
hmf17 <hmf17@mails.tsinghua.edu.cn>
2020-04-27 10:13:01 +0800

    Net Models

---
+++ SpikingModel.py
@@ -0,0 +1,26 @@
+import torch.autograd.function
+
+thresh = 0.25 # neuronal threshold
+lens = 0.5 # hyper-parameters of approximate function
+decay = 0.2 # decay constants
+
+class ActFun(torch.autograd.function):
+    @staticmethod
+    def forward(ctx, input):
+        ctx.save_for_backward(input)
+        return input.gt(thresh).float()
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        input, = ctx.saved_tensors
+        #input, = ctx.saved_tensors
+        grad_input = grad_output.clone()
+        temp = abs(input - thresh) < lens
+        return grad_input * temp.float()
+act_fun = ActFun.apply
+# ActFun.apply()
+# membrane potential update
+def mem_update(ops, x, mem, spike):
+    mem = mem * decay * (1. - spike) + ops(x)
+    spike = act_fun(mem)  # act_fun : approximation firing function
+    return mem, spike
