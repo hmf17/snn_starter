2e4785fc92483cae62f5e5eabbfd50e495ae3fcb
hmf17 <hmf17@mails.tsinghua.edu.cn>
2020-07-04 14:50:51 +0800

    Update the dataset and the dataloader

--- dataloader_test_sintel.py
+++ dataloader_test_sintel.py
@@ -1,10 +1,10 @@
 from torch.utils.data import DataLoader
 import torch
 
-from dataloader import DVSFlowDataset
+from dataloader_sintel import DVSFlowDataset
 
 if __name__ == '__main__':
-    dataset = DVSFlowDataset("/home/CBICR/hmf/dataset/use", 5, train=False, preload=False)
+    dataset = DVSFlowDataset("dataset", 5, train=False, preload=False)
     loader = DataLoader(dataset, batch_size=1)
     for i, (dvs, flow) in enumerate(loader):
         print(dvs.shape, flow.shape)
